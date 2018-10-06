a883976517d56630b6dee0877986f188f1d7f262
Rahula Palú <rahula_dd@hotmail.com>
2018-10-06 00:33:31 -0300

    arrumando bg

--- index.php
+++ index.php
@@ -46,13 +46,9 @@
       }
 
       	#bg {
-    		webkit-background-size: cover;
-            -moz-background-size: cover;
-            -o-background-size: cover;
-            background-size: cover;
-    		background-attachment: fixed;
+    		
     		background-image:linear-gradient(rgba(255, 244, 233, 1), rgba(255,244,233,0.7));
-    		background-repeat: no-repeat;
+    		background-repeat: repeat-x;
             max-height: 100%;
             max-width: 100%;
 		}
@@ -143,12 +139,7 @@ ul.thumbnails {
     .caption-box { word-break: break-all; }
     ul.thumbnails li { margin-bottom: 30px; }
 }
-@media (max-width: 489px) {
-    .carousel-control { height: 110px; line-height: 100px;} 
-}
-@media (max-width: 303px) {
-    .carousel-control { height: 149px; line-height: 138px;} 
-}
+
 
 /*For IOS*/
 @media only screen 
@@ -164,19 +155,11 @@ and (max-device-width : 479px) {
 and (max-device-width : 489px) {
     .carousel-control { height: 110px; line-height: 100px;} 
 }
-@media only screen 
-and (max-device-width : 303px) {
-     .carousel-control { height: 149px; line-height: 138px;} 
-}
 
-    /*GMAP*/
-    .mapouter{
-        text-align:center;
-    }
     .gmap_canvas {
         overflow:hidden
         ;
-
+}
 
 
 
